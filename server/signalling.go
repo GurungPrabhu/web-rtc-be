af08b0a92648f52d18ad74dae595e14989e90f12
Prabhu Gurung <prabhu.eversoft@gmail.com>
2023-10-02 09:57:36 +0545

    Feat: Init setup structure for webRTC

---
+++ server/signalling.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"net/http"
+)
+
+// CreateRoomRequestHandler Create a room and Return Room ID
+func CreateRoomRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// JoinRoomRequestHandler Join Room Request Handler
+func JoinRoomRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+}
